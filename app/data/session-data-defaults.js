ca4de10b0a907fbfc6bfa35729f188ec14f4d96e
mtuffs <mtuffs@companieshouse.gov.uk>
2022-12-20 15:50:28 +0000

    Update session-data-defaults.js

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -2,4 +2,17 @@ module.exports = {
 
   // Insert values here
 
+"email": [ "user@testcompany.co.uk" ],
+"trust-name": [ "Islands Trust" ],
+//remember to put the comma back at the end of line 25!
+//"contact-full-name": [ "Claire Contact" ],
+//"contact-email": [ "contact@testcompany.co.uk" ],
+// Set the "are we signed in?" variable (gSignedIn) to FALSE as its default
+"gSignedIn": [ "false" ],
+// signOutFirstTrigger is specific to the sign-out page to ensure the error messaging can come into play
+"signOutFirstTrigger": [ "yes" ],
+  // Overseas entity name
+"overseas-name": [ "DA INVESTMENTS LTD" ],
+// Overseas number
+"overseas-id": [ "OE001485" ]
 }
