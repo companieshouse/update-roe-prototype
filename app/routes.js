44b6e1ad7cadf8e2bda5c06dbcb0fa4da44f28a1
mtuffs <mtuffs@companieshouse.gov.uk>
2023-01-18 15:35:01 +0000

    routing updated

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,24 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 
 
+
+
 // Add your routes here - need to add from 
 
 
+// Run this code when a form is submitted to 'auth-filter'
+router.post('/v1/auth-filter', function (req, res) {
+
+    // Make a variable and give it the value from 'code-filter'
+    var codeFilter = req.session.data['code-filter']
+  
+    // Check whether the variable matches a condition
+    if (codeFilter == "yes"){
+      // Send user to next page
+      res.redirect('/v1/authentication-code')
+    } else {
+      // Send user to ineligible page
+      res.redirect('/v1/auth-get')
+    }
+  
+  })
