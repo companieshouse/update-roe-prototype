ec27472ec17fa510ad3cfa8fa504974a834217ff
mtuffs <mtuffs@companieshouse.gov.uk>
2023-02-07 09:46:34 +0000

    Routing set up

--- app/routes.js
+++ app/routes.js
@@ -6,26 +6,10 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-
-
-
-
 // Add your routes here - need to add from 
 
+ 
+router.use('/', require('./routes/v1-routes.js'))
+router.use('/', require('./routes/v2-routes.js'))
 
-// Run this code when a form is submitted to 'auth-filter'
-router.post('/v1/auth-filter', function (req, res) {
 
-    // Make a variable and give it the value from 'code-filter'
-    var codeFilter = req.session.data['code-filter']
-  
-    // Check whether the variable matches a condition
-    if (codeFilter == "yes"){
-      // Send user to next page
-      res.redirect('/v1/authentication-code')
-    } else {
-      // Send user to ineligible page
-      res.redirect('/v1/auth-get')
-    }
-  
-  })
---
+++ app/routes/v1-routes.js
@@ -0,0 +1,25 @@
+
+
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+    
+    
+
+    // Run this code when a form is submitted to 'auth-filter'
+router.post('/v1/auth-filter', function (req, res) {
+
+    // Make a variable and give it the value from 'code-filter'
+    var codeFilter = req.session.data['code-filter']
+  
+    // Check whether the variable matches a condition
+    if (codeFilter == "yes"){
+      // Send user to next page
+      res.redirect('/v1/authentication-code')
+    } else {
+      // Send user to ineligible page
+      res.redirect('/v1/auth-get')
+    }
+  
+  })
+
+module.exports=router;
