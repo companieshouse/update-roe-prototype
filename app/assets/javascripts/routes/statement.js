2750d08c34ac586b826c90d60fbb58dd4520e0a1
mtuffs <mtuffs@companieshouse.gov.uk>
2023-02-17 09:59:39 +0000

    Update statement.js

--- app/assets/javascripts/routes/statement.js
+++ app/assets/javascripts/routes/statement.js
@@ -1,8 +1,8 @@
 function postStatementFilter (router) {
     router.post( '/v2/check-your-answers', function ( req, res ) {
-          const secureRegister = req.body;    
-          if ( secureRegister["boStatement"] === "no" ) {      
-              res.redirect( "/v2/beneficial-owners/involved-types-test" );
+          const boStatement = req.body;    
+          if ( boStatement ["boStatement"] === "no" ) {      
+              res.redirect( "/v2/beneficial-owner/involved-types-test" );
           } else {
             res.redirect( "/v2/check-your-answers" );
           }
