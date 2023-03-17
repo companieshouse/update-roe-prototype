e868a261a5a29f2a58e9b96ab21777256397e694
mtuffs <mtuffs@companieshouse.gov.uk>
2023-03-17 15:59:11 +0000

    error messaging

--- app/routes/v4-routes.js
+++ app/routes/v4-routes.js
@@ -160,6 +160,101 @@ router.post('/v4/beneficial-owner/bo-individual', function (req, res) {
 
 
 
+
+
+
+
+
+// ******* mo-individual validation ********************************
+router.get('/v4/beneficial-owner/mo', function (req, res) {
+  // Set URl
+  res.render('v4/beneficial-owner/mo', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v4/beneficial-owner/mo', function (req, res) {
+  // Create variables
+  var propertyError = false;
+  var address1Error = false;
+  var cityError = false;
+  var countryError = false;
+  var moCeasedError = false;
+  var errors = [];
+
+  // Check if user has filled out a value
+  if (req.session.data['managing-officer-usual-residential-address-property-name-number'] === '') {
+    // No value so add error to array
+    propertyError = true;
+    errors.push({
+      text: 'Enter the property name or number',
+      href: '#managing-officer-usual-residential-address-property-name-number'
+    })
+  }
+
+  // Check if user has filled out a value
+  if (req.session.data['managing-officer-usual-residential-address-line-1'] === '') {
+    // No value so add error to array
+    address1Error = true;
+    errors.push({
+      text: 'Enter address line 1',
+      href: '#managing-officer-usual-residential-address-line-1'
+    })
+  }
+
+  // Check if user has filled out a value
+  if (req.session.data['managing-officer-usual-residential-address-city-town'] === '') {
+    // No value so add error to array
+    cityError = true;
+    errors.push({
+      text: 'Enter the city or town',
+      href: '#managing-officer-usual-residential-address-city-town'
+    })
+  }
+
+  // Check if user has filled out a value
+  if (req.session.data['country'] === '') {
+    // No value so add error to array
+    countryError = true;
+    errors.push({
+      text: 'Enter the country',
+      href: '#country'
+    })
+  }
+
+  // Check if user has filled out a value
+  if (typeof req.session.data['mo-ceased'] === 'undefined') {
+    // No value so add error to array
+   moCeasedError = true;
+    errors.push({
+      text: 'Select yes if they are still a registrable beneficial owner',
+      href: '#mo-ceased'
+    })
+  }
+    
+  if (errors.length != 0) {
+  // Re-show page with error value as true so errors will show
+    res.render('v4/beneficial-owner/mo', {
+      errorMoCeased: moCeasedError,
+      errorProperty: propertyError,
+      errorAddress1: address1Error,
+      errorCity: cityError,
+      errorCountry: countryError,
+      errorList: errors
+    })
+  } else {
+    // User inputted value so move to next page
+    res.redirect('/v4/beneficial-owner/involved-types')
+  }
+})
+
+
+
+
+
+
+
+
 // router.post('/v4/beneficial-owner/bo-individual', function (req, res) {
 //   // Create empty array
 //   var errors = []
