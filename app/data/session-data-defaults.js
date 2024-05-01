9b1ba8ded43cc8533bb08dce917c0cc1816af3b6
mtuffs <mtuffs@companieshouse.gov.uk>
2024-05-01 09:56:14 +0100

    cya updated

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -5,8 +5,8 @@ module.exports = {
 "email": [ "user@testcompany.co.uk" ],
 "trust-name": [ "Islands Trust" ],
 //remember to put the comma back at the end of line 25!
-//"contact-full-name": [ "Claire Contact" ],
-//"contact-email": [ "contact@testcompany.co.uk" ],
+"contact-full-name": [ "Claire Contact" ],
+"contact-email": [ "contact@testcompany.co.uk" ],
 // Set the "are we signed in?" variable (gSignedIn) to FALSE as its default
 "gSignedIn": [ "false" ],
 // signOutFirstTrigger is specific to the sign-out page to ensure the error messaging can come into play
